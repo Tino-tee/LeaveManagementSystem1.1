572fc9453454bed739844d75c3e35a11e73de545
Tino-tee <chitimatinotenda55@gmail.com>
2026-06-05 14:46:10 +0200

    Completed skeleton of Leave management system.

--- LeaveManagementSystem1.1/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem1.1/Data/ApplicationDbContext.cs
@@ -3,7 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagementSystem1._1.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
     }
 }
